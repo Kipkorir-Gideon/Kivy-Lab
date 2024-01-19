cbf333df707e3744a68e6d775aa86eba2bf4e587
Ngetich Gideon <ngetichgideon21@gmail.com>
2024-01-19 23:17:01 +0300

    canvas in the Python code.

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
 from kivy.graphics.context_instructions import Color
+from kivy.metrics import dp
 
-from kivy.properties import StringProperty, NumericProperty, BooleanProperty
+from kivy.properties import StringProperty, NumericProperty, BooleanProperty, Clock
 
 
 
@@ -74,6 +75,53 @@ class CanvasExample4(Widget):
             Color(0, 1, 0)
             Line(circle =(400, 200, 80), width=2)
             Line(rectangle =(700, 500, 150, 100), width=2)
+            self.rect = Rectangle(pos=(700, 200), size=(150, 100))
+            
+    def on_button_a_click(self): 
+        x, y = self.rect.pos
+        w, h = self.rect.size
+        inc = dp(10)
+        
+        diff = self.width - (x+w)
+        if diff < inc:
+            inc = diff
+        x += inc
+        self.rect.pos = (x, y)
+        
+        
+class CanvasExample5(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size = dp(50)
+        self.vx = dp(3)
+        self.vy = dp(4) 
+        Clock.schedule_interval(self.update, 1/60)
+        with self.canvas:
+            self.ball = Ellipse(pos=(100, 100), size=(self.ball_size, self.ball_size))
+            
+    def on_size(self, *args):
+        self.ball.pos = (self.center_x-self.ball_size/2, self.center_y-self.ball_size/2)
+        
+    def update(self, dt):
+        x, y = self.ball.pos
+        
+        x += self.vx
+        y += self.vy
+        
+        if y + self.ball_size > self.height:
+            y = self.height - self.ball_size
+            self.vy = -self.vy
+        if x + self.ball_size > self.width:
+            x = self.width - self.ball_size
+            self.vx = -self.vx
+        if y < 0:
+            y = 0
+            self.vy = -self.vy
+        if x < 0:
+            x = 0
+            self.vx = -self.vx
+            
+        self.ball.pos = (x, y)
 
 
 
